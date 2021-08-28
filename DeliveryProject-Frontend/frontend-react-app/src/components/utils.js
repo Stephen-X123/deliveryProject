9d96cfae6b61dd242ce6037508fe05770054a71e
StephenX123 <sx48@cornell.edu>
2021-08-28 03:13:43 -0400

    new file names

--- DeliveryProject-Frontend/frontend-react-app/src/components/utils.js
+++ DeliveryProject-Frontend/frontend-react-app/src/components/utils.js
@@ -2,9 +2,9 @@
   Imagining which services will be required from the backend.
 
   url, method, purpose
-  /login, POST, login
-  /register, POST, register
-  /logout, POST, logout
+  /login, POST, login 1
+  /register, POST, register 1
+  /logout, POST, logout 1
   /order, POST, send order information to BE (backend return order ok)
   if not -> proceed to order error.
   /user, GET, get user info (which has firstName, lastName, password, order history etc)
