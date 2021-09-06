9fe0937c350a747f8ee713c42e1718e7a2cd3526
StephenX123 <sx48@cornell.edu>
2021-09-06 02:48:17 -0400

    Changed nav.js to switch between order and logout buttons

--- DeliveryProject-Frontend/frontend-react-app/src/components/Order.js
+++ DeliveryProject-Frontend/frontend-react-app/src/components/Order.js
@@ -1,10 +1,17 @@
 import React from "react";
-import '../css/Order.css'
+import '../css/Order.css';
+import { Steps } from 'antd';
+const { Step } = Steps
 
 export default class Order extends React.Component {
   render() {
     return (
       <div className="order-background">
+        <Steps current={0} onChange={this.onChange} direction="vertical" className="steps">
+          <Step title="Step 1" description="Address + Package + Pickup time" />
+          <Step title="Step 2" description="Payment Information" />
+          <Step title="Step 3" description="Order Confirmation" />
+        </Steps>
       </div>
     );
   }
