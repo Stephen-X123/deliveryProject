9201f095f99c1f07d90ed4abd0b7bad9af375d99
WM <62089599+TruckerTaco@users.noreply.github.com>
2021-09-09 02:19:19 -0700

    Complete tracking page

--- DeliveryProject-Frontend/frontend-react-app/src/components/OrderHistory.js
+++ DeliveryProject-Frontend/frontend-react-app/src/components/OrderHistory.js
@@ -1,19 +1,6 @@
 import React from "react";
 import '../css/OrderHistory.css'
 import { Link } from "react-router-dom";
-
-// export default class OrderHistory extends React.Component {
-//   render() {
-//     return (
-//       <div className="order-history-background">
-//         <h1 className="order-history-title">
-//           Order history
-//         </h1>
-//       </div>
-//     );
-//   }
-// }
-
 import '../css/OrderHistory.css'
 import { List, Row, Col, Button } from 'antd';
 import Item from "antd/lib/list/Item";
@@ -41,35 +28,35 @@ class OrderHistory extends React.Component {
       <>
         <h1 style = {{left : '10vw', position : 'absolute', top : '15vh'}}>Order History</h1>
         <List
-        style = {{marginTop: '10vh', borderColor: 'transparent'}}
+        style = {{marginTop: '10vh', borderColor : 'transparent'}}
         size="large"
         //header={<div><h3 style = {{left : '10vw', position : 'absolute', marginBottom : '30vh', top : '0vh'}}>Order History</h3></div>}
         bordered
         dataSource={data}
-        renderItem={item => <div style = {{width : '77vw' , height : '20vh', backgroundColor : "grey", 
+        renderItem={item => <div style = {{width : '77vw' , height : '20vh', backgroundColor : 'whitesmoke', 
         marginTop : '5vh', borderRadius : '20px', left : '10vw', position : "relative"}}><List.Item>{
           <>
           <span style = {{marginBottom : '1vh'}}> 
             <Row justify="space-between">
-              <Col><h5 style = {{left : '0vw', postion : 'absolute'}}>Order Number {item.order_number}</h5></Col>
-              <Col><h5 style = {{left : '30vw', postion : 'absolute'}}>From {item.sender}</h5></Col>
-              <Col><h5 style = {{right: '0vw', postion : 'absolute'}}>To {item.receiver}</h5></Col>
+              <Col><h4 style = {{left : '0vw', postion : 'absolute'}}><b>Order Number</b> {item.order_number}</h4></Col>
+              <Col><h4 style = {{left : '30vw', postion : 'absolute'}}><b>From</b> {item.sender}</h4></Col>
+              <Col><h4 style = {{right: '0vw', postion : 'absolute'}}><b>To</b> {item.receiver}</h4></Col>
             </Row>
             <hr style = {{width : '73vw', color : 'black', left : '-5vw'}}/>
             <Row justify="space-between">
               <span>
-                <h5 style = {{display : 'inline', marginRight : '10vw'}}>{item.order_status}</h5>
-                <h5 style = {{display : 'inline'}}>Pick Up Time {item.actual_pick_up_time}</h5>
+                <h2 style = {{display : 'inline', marginRight : '10vw'}}>{item.order_status}</h2>
+                <h2 style = {{display : 'inline'}}>Pick Up Time {item.actual_pick_up_time}</h2>
               </span>
               <span>
                 <Col>
                   <Link to = "/tracking">
-                            <h5>
+                            <h5 style = {{color : 'blue'}}>
                                 Track My Order
                             </h5>
                   </Link>
                   <Link to = "/orderdetails">
-                            <h5>
+                            <h5 style = {{color : 'blue'}}>
                                 Order Details
                             </h5>
                   </Link>
@@ -77,26 +64,7 @@ class OrderHistory extends React.Component {
               </span>
               </Row>
           </span>
-         
-
-          {/* <span style = {{marginBottom : '1vh'}}> 
-            <h2>"Order Number " {item.order_number}</h2>
-            <h2>"From " {item.sender}</h2>
-            <h2>"To " {item.receiver}</h2> 
-          </span> */}
-          
-          
-
-          {/* <div><hr style = {{color : "black", zIndex : '100', width : '80vw'}}/></div> */}
           </>
-
-
-          
-          
-          
-          
-          
-          
           }</List.Item></div>}
         />
       </>
